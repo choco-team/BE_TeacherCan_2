2dcc17c635c73c72b0f9eec0cc3f0a54e980990d
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-03-10 22:25:14 +0900

    정적 호스팅 제거

--- src/main.ts
+++ src/main.ts
@@ -41,20 +41,6 @@ if (process.env.LOCAL==="true"){
 }
 
 
-
-  // // ✅ 정적 파일 서빙 - 제거 예정
-  // app.use(express.static(join(process.cwd(), "front")));
-
-  // // ✅ API 요청이 아닌 경우 index.html 제공 (Svelte 라우팅 지원) - 제거 예정
-  // app.use((req, res, next) => {
-  //   if (req.originalUrl.startsWith("/api")) {
-  //     return next();
-  //   }
-  //   res.sendFile(join(process.cwd(), "front", "index.html"));
-  // });
-
-
-
   // ✅ 쿠키 파서를 전역 미들웨어로 추가
   app.use(cookieParser());
   // ✅ 글로벌 파이프 설정 (DTO 유효성 검사)
