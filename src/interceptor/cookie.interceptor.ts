9e97a5c021cb21c94be5da0b07ee7fc9b59bcbc5
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-03-14 23:57:58 +0900

    fix:다른 도메인에서 쿠키 전송을 허용하여 Nextjs에 쿠키를 사용할 수 있도록 허용합니다

--- src/interceptor/cookie.interceptor.ts
+++ src/interceptor/cookie.interceptor.ts
@@ -16,7 +16,7 @@ export class CookieInterceptor implements NestInterceptor {
           response.cookie('sessionId', request.sessionId, {
             httpOnly: true,
             secure: !process.env.LOCAL,
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 1000 * 60 * 60 * 24 * 7, // 7일 유지
           });
         }
