a4617114eca6d42963fb28c7b68ed9437e49516a
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-03-23 18:27:06 +0900

    fix:CSRF토큰을 헤더에서 바르게 읽습니다

--- src/middleware/csrf.middleware.ts
+++ src/middleware/csrf.middleware.ts
@@ -10,8 +10,8 @@ export class CsrfMiddleware implements NestMiddleware {
     }
     
     const csrfCookie = req.cookies['X-CSRF-Token'];
-    const csrfHeader = req.headers['X-CSRF-Token'];
-    
+    const csrfHeader = req.headers['x-csrf-token'];
+        
     // 토큰이 없거나 일치하지 않으면 오류
     if (!csrfCookie || !csrfHeader || csrfCookie !== csrfHeader) {
       throw new UnauthorizedException('CSRF 토큰이 유효하지 않습니다');
