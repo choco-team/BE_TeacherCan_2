9d4d33f9c0dc7ad2fe01ca89885342dbe513b8ba
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-04-14 02:01:45 +0900

    Revert "Dev"

---
+++ src/interceptor/cookie.interceptor.ts
@@ -0,0 +1,27 @@
+import { CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Response } from 'express';
+import { map } from 'rxjs';
+import { AuthRequest } from 'src/auth/auth.types';
+
+@Injectable()
+export class CookieInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler) {
+    const ctx = context.switchToHttp();
+    const request = ctx.getRequest<AuthRequest>();
+    const response = ctx.getResponse<Response>();
+
+    return next.handle().pipe(
+      map((data) => {
+        if (request.sessionId) {
+          response.cookie('sessionId', request.sessionId, {
+            httpOnly: true,
+            secure: process.env.LOCAL!=='true',
+            sameSite: 'none',
+            maxAge: 1000 * 60 * 60 * 24 * 7, // 7일 유지
+          });
+        }
+        return data;
+      }),
+    );
+  }
+}
