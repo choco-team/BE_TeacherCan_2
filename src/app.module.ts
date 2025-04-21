b2257485cc9ecf939ae2625deaddb085b5744b3f
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-04-21 22:08:17 +0900

    Revert "Refactor/cache CICD test코드를 수정합니다"

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,27 +2,31 @@ import { Module, NestModule, MiddlewareConsumer } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppDataSource } from './db/AppDataSource';
 import { AuthModule } from './auth/auth.module';
+import { SubjectModule } from './subject/subject.module';
+import { QuestionModule } from './question/question.module';
 import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AuthGuard } from './auth/auth.guard';
 import { RolesGuard } from './auth/role.guard';
 import { StudentModule } from './student/student.module';
+import { LlmModule } from './llm/llm.module';
 import { MusicModule } from './music/music.module';
 import { APP_INTERCEPTOR } from '@nestjs/core';
 import { CsrfInterceptor } from './interceptor/csrf.interceptor';
 import { CsrfMiddleware } from './middleware/csrf.middleware';
-import { RedisModule } from './redis/redis.module';
 
 @Module({
   imports: [
     TypeOrmModule.forRoot(AppDataSource.options), // AppDataSource 적용
     AuthModule, // ✅ `AuthModule`을 통해 `AuthGuard`, `RolesGuard` 제공
+    SubjectModule,
+    QuestionModule,
     ConfigModule.forRoot({
       isGlobal: true, // ✅ 전역 사용 가능하도록 설정
     }),
     StudentModule,
+    LlmModule,
     MusicModule,
-    RedisModule
   ],
   providers: [
     {
--- src/music/music.module.ts
+++ src/music/music.module.ts
@@ -5,13 +5,14 @@ import { Music } from 'src/db/entities/music.entity';
 import { Room } from 'src/db/entities/room.entity';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { RsaKey } from 'src/db/entities/rsaKey.entity';
+import { MusicInfoService } from './musicInfo.service';
+import { MusicRoomService } from './musicRoom.service';
+import { MusicStudentService } from './musicStudent.service';
 import { CryptoModule } from 'src/services/crypto.module';
-import { MusicSQLService } from './music.sql.service';
-import { MusicService } from './music.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Student, Music, Room, RsaKey]), CryptoModule],
-  providers: [MusicSQLService, MusicService],
+  providers: [MusicInfoService, MusicRoomService, MusicStudentService],
   controllers: [MusicController]
 })
 export class MusicModule {}
