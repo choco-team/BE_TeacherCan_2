307eb45ea1de142c94cd5bb276a739b088ac28e4
SuperBallista <140693798+SuperBallista@users.noreply.github.com>
2025-05-01 20:12:00 +0900

    fix: CloudSQL 연결설정을 테스트용과 실제용으로 구분합니다

--- src/db/AppDataSource.ts
+++ src/db/AppDataSource.ts
@@ -7,18 +7,26 @@ import { Room } from './entities/room.entity';
 import { Student } from './entities/student.entity';
 import { Music } from './entities/music.entity';
 
-ConfigModule.forRoot(); // 환경 변수 로드
+ConfigModule.forRoot(); // .env 로드
+
+const isLocal = process.env.LOCAL === 'true';
 
 export const AppDataSource = new DataSource({
-  type: 'mysql', // 또는 'mariadb'
-  host: process.env.DATABASE_HOST || 'localhost',
-  port: Number(process.env.DATABASE_PORT) || 3306,
+  type: 'mysql',
+  ...(isLocal
+    ? {
+        host: process.env.DATABASE_HOST || 'localhost',
+        port: Number(process.env.DATABASE_PORT) || 3306,
+      }
+    : {
+        socketPath: process.env.DB_SOCKET_PATH || '/teachercan',
+      }),
   username: process.env.DATABASE_USER || 'root',
   password: process.env.DATABASE_PASSWORD || 'yourpassword',
   database: process.env.DATABASE_NAME || 'mydatabase',
-  entities: [User, Session,  RsaKey, Room, Student, Music], // 엔티티 자동 로드
-  logging: process.env.LOCAL === 'true', // 개발 환경에서만 로그 활성화
+  entities: [User, Session, RsaKey, Room, Student, Music],
+  logging: isLocal,
   extra: {
-    authPlugins: 'caching_sha2_password'
-  }
+    authPlugins: 'caching_sha2_password',
+  },
 });
